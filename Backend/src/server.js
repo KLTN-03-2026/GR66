6bd30b60cc36fe2b5da6180106477a6b893a5fc0
Phạm Văn Bảo <baopham9424@gmail.com>
2026-03-27 09:04:25 +0700

    bao

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 const app = express();
@@ -7,9 +8,13 @@ const PORT = process.env.PORT || 5001;
 //middleware 
 app.use(express.json()); // cho phép server đọc Json
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
+
 
 
 
