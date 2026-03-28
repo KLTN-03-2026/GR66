fd482282f6d10a5280dc7264cb2d56abaae21f43
FrenkieVo <vohothanhtrung@gmail.com>
2026-03-28 17:43:33 +0700

    Trung update Home100%

--- frontend/components/Introsection.tsx
+++ frontend/components/Introsection.tsx
@@ -9,6 +9,7 @@ export default function IntroSection() {
     <div className="relative min-h-screen overflow-hidden">
       {/* Background Video */}
       <video
+        key="home-video"
         autoPlay
         muted
         loop
@@ -16,7 +17,7 @@ export default function IntroSection() {
         preload="auto"
         className="absolute inset-0 w-full h-full object-cover"
       >
-        <source src="VideoBanner.mp4" type="video/mp4" />
+        <source src="/VideoBanner.mp4" type="video/mp4" />
         <img
           src="/intro.jpg"
           alt="Exceptional journeys"
